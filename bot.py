ce9606fe8e47dd0cb1286b9fc6d34eaf6546f5b9
MikkoBoy <mikko.ahonen666@gmail.com>
2020-11-09 23:03:39 +0200

    Status

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ client = discord.Client()
 @client.event
 async def on_ready():
   print("Logged in")
+  game = discord.Game("with myself")
+  await client.change_presence(activity=game)
 
 @client.event
 async def on_message(message):
@@ -14,5 +16,15 @@ async def on_message(message):
 
     if message.content.startswith('+hello'):
         await message.channel.send('Hello!')
+    # Wait for esimerkki, tsekkaa api
+    # if message.content.startswith('+greet'):
+    #     channel = message.channel
+    #     await channel.send('Say hello!')
+
+    #     def check(m):
+    #         return m.content == 'hello' and m.channel == channel
+
+    #     msg = await client.wait_for('message', check=check)
+    #     await channel.send('Hello {.author}!'.format(msg))
 
 client.run(cfg.token)
